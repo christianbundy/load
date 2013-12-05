6bdcfefda7012cb8266aa951b908e26b34b332fc
Christian Bundy <me@christianbundy.com>
2013-12-05 00:23:53 -0600

    Add owner/repo GitHub shortcut, close #1

--- lib/load.rb
+++ lib/load.rb
@@ -1,5 +1,10 @@
 module Load
-	def self.load(repo, path)
-    system "git clone --recursive #{repo} #{path}"
+	def self.load(source, target)
+		if source.index(':') == nil and source.index('@') == nil
+			repo = "git@github.com:#{source}.git"
+		else
+			repo = source;
+		end
+    system "git clone --recursive #{repo} #{target}"
   end
 end
