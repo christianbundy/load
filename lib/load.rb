06dd9041304144f8a034694ed5f811381a1fa47b
Christian Bundy <me@christianbundy.com>
2013-12-05 00:35:05 -0600

    Add help text when being run without arguments

--- lib/load.rb
+++ lib/load.rb
@@ -1,10 +1,14 @@
 module Load
+	def self.help
+		puts "Usage: \n" +
+		"$ load christianbundy/load"
+	end
 	def self.load(source, target)
 		if source.index(':') == nil and source.index('@') == nil
 			repo = "git@github.com:#{source}.git"
 		else
 			repo = source;
 		end
-    system "git clone --recursive #{repo} #{target}"
-  end
+		system "git clone --recursive #{repo} #{target}"
+	end
 end
